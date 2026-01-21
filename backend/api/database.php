295b5ae95f19116231e3c8b616b60360f48e7a82
Brian <nguyenbao12122005@gmail.com>
2026-01-21 19:22:41 +0700

    Update database connection

--- backend/api/database.php
+++ backend/api/database.php
@@ -1,13 +1,36 @@
 <?php
-$config = require __DIR__ . '/../config/database.php';
+// $config = require __DIR__ . '/../config/database.php';
+
+// try {
+//     $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['name']};charset=utf8mb4";
+//     $conn = new PDO($dsn, $config['user'], $config['pass'], [
+//         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+//     ]);
+// } catch (PDOException $e) {
+//     die("DB error: " . $e->getMessage());
+// }
+
 
 try {
-    $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['name']};charset=utf8mb4";
-    $conn = new PDO($dsn, $config['user'], $config['pass'], [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
+    $dsn = "mysql:host={$_ENV['MYSQLHOST']};port={$_ENV['MYSQLPORT']};dbname={$_ENV['MYSQL_DATABASE']};charset=utf8mb4";
+
+    $conn = new PDO(
+        $dsn,
+        $_ENV['MYSQLUSER'],
+        $_ENV['MYSQLPASSWORD'],
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    );
+
 } catch (PDOException $e) {
-    die("DB error: " . $e->getMessage());
+    http_response_code(500);
+    echo json_encode([
+        "status" => 500,
+        "summary" => "DB error",
+        "detailed" => $e->getMessage()
+    ]);
+    exit;
 }
 
 ?>
