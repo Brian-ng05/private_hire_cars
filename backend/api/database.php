2e3ab8a2fbd43f1f24858b5a4c7179c7b8fed795
Brian <nguyenbao12122005@gmail.com>
2026-01-21 23:06:07 +0700

    Check env

--- backend/api/database.php
+++ backend/api/database.php
@@ -1,14 +1,7 @@
 <?php
-// $config = require __DIR__ . '/../config/database.php';
 
-// try {
-//     $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['name']};charset=utf8mb4";
-//     $conn = new PDO($dsn, $config['user'], $config['pass'], [
-//         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-//     ]);
-// } catch (PDOException $e) {
-//     die("DB error: " . $e->getMessage());
-// }
+var_dump(getenv());
+exit;
 
 
 $host = getenv('MYSQLHOST');
