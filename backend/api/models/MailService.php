de9f3af6eeaf21737bdacd9a01b9b7f424e9f9c8
Brian <nguyenbao12122005@gmail.com>
2026-01-23 12:49:19 +0700

    Add log to fix bug

--- backend/api/models/MailService.php
+++ backend/api/models/MailService.php
@@ -8,7 +8,7 @@ class MailService
     {
         $data = [
             "from" => ($_ENV['MAIL_NAME'] ?? 'Private Hire Car') .
-                      " <" . ($_ENV['MAIL_FROM'] ?? 'no-reply@resend.dev') . ">",
+                " <" . ($_ENV['MAIL_FROM'] ?? 'no-reply@resend.dev') . ">",
             "to" => [$to],
             "subject" => $subject,
             "html" => $html
@@ -29,12 +29,13 @@ class MailService
         $response = curl_exec($ch);
 
         if (curl_errno($ch)) {
-            error_log("Mail API error: " . curl_error($ch));
+            error_log("Curl error: " . curl_error($ch));
             return false;
         }
 
         $result = json_decode($response, true);
 
+        error_log("Resend response: " . $response);
         return isset($result['id']);
     }
 }
