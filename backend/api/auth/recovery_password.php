47f4f75a59c199a2974029aa805e612730d8e2c7
Brian <nguyenbao12122005@gmail.com>
2026-01-24 00:31:02 +0700

    Fix wrong file database directory

--- backend/api/auth/recovery_password.php
+++ backend/api/auth/recovery_password.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../utils/api_common.php';
 require_once __DIR__ . '/../utils/validator.php';
-require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../database.php';
 require_once __DIR__ . '/../models/User.php';
 require_once __DIR__ . '/../models/EmailVerifications.php';
 
